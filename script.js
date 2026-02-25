6c7232330b7a43be527522f5a5f2e61e3c1f8d1b
Islambek <i.almas4067@gmail.com>
2026-02-25 08:05:59 +0500

    Refactor weather data fetching and UI updates

--- script.js
+++ script.js
@@ -1,43 +1,71 @@
-const date = document.querySelector('.data')
-const country = document.querySelector('.country')
-const rightWeather = document.querySelector('.right-weather')
-const temperatureBg = document.querySelector('.temperature-bg')
-const rightWeek = document.querySelector('.right-week')
-const humidity = document.querySelector('.humidity')
-const wind = document.querySelector('.wind')
-const precipitation = document.querySelector('.precipitation')
-const getData = async () =>{
-  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,showers,wind_speed_10m,relative_humidity_2m,precipitation&models=icon_seamless&current=rain,showers,snowfall,precipitation&timezone=auto&wind_speed_unit=mph&precipitation_unit=inch')
-  const data = await response.json()
-  wind.innerText = `${data.hourly.wind_speed_10m[0]} mph` 
-  humidity.innerText = `${data.hourly.relative_humidity_2m[0]} %`
-  precipitation.innerText = `${data.hourly.precipitation[0]} inch`
-  
+const inputSearch = document.querySelector(".search");
+const date = document.querySelector(".data");
 
-  const currentDate = new Date();
-  console.log(data);
+const rightWeather = document.querySelector(".right-weather");
+const temperatureBg = document.querySelector(".temperature-bg");
+const rightWeek = document.querySelector(".right-week");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const precipitation = document.querySelector(".precipitation");
+const btn = document.querySelector(".search-btn");
+const section = document.querySelector('section')
 
-const iso = currentDate.toISOString().slice(0, 13) + ":00";
-
-const weatherTime = data.hourly.time.find(t => t === iso);
-
-const weatherDate = new Date(weatherTime);
-
-const formatted = weatherDate.toLocaleDateString("en-US", {
-  month: "long",
-  day: "numeric",
-  year: "numeric"
+const newDate = new Date().toLocaleDateString("en-US", {
+  weekday: "short",
 });
-  date.insertAdjacentHTML('beforeend', `
-    <p>${formatted}</p>
-    `)
-    country.innerHTML = data.timezone.split('/').join(', ')
-  temperatureBg.insertAdjacentHTML('beforeend',  `
-    <span class="text-5xl font-bold text-white">${data.hourly.temperature_2m[0] + data.hourly_units.temperature_2m}</span>
-    `)
+rightWeek.innerHTML = newDate;
 
+
+const getInf = async (name) => {
+  const res = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${name}&count=10&language=en&format=json`,
+  );
+  const dataS = await res.json();
+  console.log(dataS);
+  const getData = async (longitude , latitude) => {
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,showers,wind_speed_10m,relative_humidity_2m,precipitation&models=icon_seamless&current=rain,showers,snowfall,precipitation&timezone=auto&wind_speed_unit=mph&precipitation_unit=inch`,
+    );
+    const data = await response.json();
     
-    for(let i = 3; i<11; i++){
+  
+   
+    wind.innerText = `${data.hourly.wind_speed_10m[0]} mph`;
+    humidity.innerText = `${data.hourly.relative_humidity_2m[0]} %`;
+    precipitation.innerText = `${data.hourly.precipitation[0]} inch`;
+
+    const currentDate = new Date();
+    console.log(data);
+
+    const iso = currentDate.toISOString().slice(0, 13) + ":00";
+
+    const weatherTime = data.hourly.time.find((t) => t === iso);
+
+    const weatherDate = new Date(weatherTime);
+
+    const formatted = weatherDate.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+    date.insertAdjacentHTML(
+      "beforeend",
+      `
+      <h1>${data.timezone}</h1>
+    <p>${formatted}</p>
+    `,
+    );
+    
+    
+    // .split("/").join(", ");
+    temperatureBg.insertAdjacentHTML(
+      "beforeend",
+      `
+    <span class="text-5xl font-bold text-white">${data.hourly.temperature_2m[0] + data.hourly_units.temperature_2m}</span>
+    `,
+    );
+
+    for (let i = 3; i < 11; i++) {
       rightWeather.innerHTML += `
        <div class="flex items-center justify-between bg-neutral-400 px-3 rounded-lg ">
           <div class="flex gap-3 items-center">
@@ -46,12 +74,22 @@ const formatted = weatherDate.toLocaleDateString("en-US", {
           </div>
           <span>${data.hourly.temperature_2m[i] + data.hourly_units.temperature_2m}</span>
         </div>
-      `
+      `;
     }
-}
-getData()
+  };
+  getData(dataS.results[0].longitude , dataS.results[0].latitude);
+};
 
-const newDate = new Date().toLocaleDateString('en-US',{
-  weekday: 'short'
-})
-rightWeek.innerHTML = newDate
+
+btn.addEventListener("click", () => {
+  
+  date.innerHTML = '';
+  temperatureBg.innerHTML = '';
+  rightWeather.innerHTML = '';
+ 
+  wind.innerText = ''
+    humidity.innerText = ''
+    precipitation.innerText = '';
+
+  getInf(inputSearch.value);
+});
